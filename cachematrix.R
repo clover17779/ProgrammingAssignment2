6ea0fdfada2f6e478148b718cb3bedd78fb96ea0
clover17779 <1533906714@qq.com>
2015-04-22 17:43:26 +0800

    my_cachematrix add describtion .R
    my_cachematrix add describtion

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,13 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## a short comment describing this function£º
+##this function can get a special matrix which just is a list
+##it can not only set and get the matrix  but also set and get the inverse matrix(wich call "ni")
+##you can found their environments are same
+##in fact I just do by imitation and I dont kown it is right or not
+##if you know the right answer, please tell me in the comment box 
+##thank you very much 
 makeCacheMatrix <- function(x = matrix()) {
     ni <- NULL
 
@@ -20,6 +25,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+## this function may can check that wheter the matrix has been compute its inverse matrix
+## if it has done it this function can return the result from cache
+## else it will compute this matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
